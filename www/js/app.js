363a8d70f579bf2669b1d7c6c7c1083ec1e5d98d
Ryuta Hamasaki <hamasaki@kurashicom.jp>
2015-05-18 00:57:58 +0900

    configure

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,40 @@ angular.module('app', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+  .state('tab.blog', {
+    url: '/blog',
+    views: {
+      'tab-blog': {
+        templateUrl: 'templates/tab-blog.html'
+      }
+    }
+  })
+  .state('tab.instagram', {
+    url: '/instagram',
+    views: {
+      'tab-instagram': {
+        templateUrl: 'templates/tab-instagram.html'
+      }
+    }
+  })
+  .state('tab.twitter', {
+    url: '/twitter',
+    views: {
+      'tab-twitter': {
+        templateUrl: 'templates/tab-twitter.html'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/tab/blog');
+
+});
