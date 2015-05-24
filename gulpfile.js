55c42cb597b61a380e9d9097f347220daf57f373
Ryuta Hamasaki <hamasaki@kurashicom.jp>
2015-05-24 15:14:06 +0900

    install angular-fire library

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,18 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var sh = require('shelljs');
+var jslibs = [
+    'bower_components/firebase/firebase.js',
+    'bower_components/angularfire/dist/angularfire.min.js'
+];
 
-gulp.task('default', ['watch-sass', 'watch-js-concat', 'watch-js-uglify']);
+gulp.task('default', [
+    'watch-sass',
+    'watch-concat-js-main',
+    'watch-uglify-js-main',
+    'watch-concat-js-lib',
+    'watch-uglify-js-lib'
+]);
 
 // exec tasks
 gulp.task('sass', function(done) {
@@ -26,18 +36,35 @@ gulp.task('sass', function(done) {
         .on('end', done);
 });
 
-gulp.task('concat-js', function(done) {
+gulp.task('concat-js-main', function(done) {
     gulp.src(['www/src/**/boot.js', 'www/src/**/*.js', '!www/src/**/*_test.js'])
         .pipe(concat('main.js', {newLine: '\n\n\n'}))
         .pipe(gulp.dest('www/dist/js'))
         .on('end', done);
 });
 
-gulp.task('uglify-js', function(done) {
+gulp.task('uglify-js-main', function(done) {
     gulp.src('www/dist/js/main.js')
         .pipe(sourcemaps.init())
             .pipe(uglify())
             .pipe(concat('main.min.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('www/dist/js'))
+        .on('end', done);
+});
+
+gulp.task('concat-js-lib', function(done) {
+    gulp.src(jslibs)
+        .pipe(concat('dependencies.js', {newLine: '\n\n\n'}))
+        .pipe(gulp.dest('www/dist/js'))
+        .on('end', done);
+});
+
+gulp.task('uglify-js-lib', function(done) {
+    gulp.src('www/dist/js/dependencies.js')
+        .pipe(sourcemaps.init())
+            .pipe(uglify())
+            .pipe(concat('dependencies.min.js'))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('www/dist/js'))
         .on('end', done);
@@ -48,10 +75,18 @@ gulp.task('watch-sass', function() {
     gulp.watch('scss/**/*.scss', ['sass']);
 });
 
-gulp.task('watch-js-concat', function() {
-    gulp.watch('www/src/**/*.js', ['concat-js']);
+gulp.task('watch-concat-js-main', function() {
+    gulp.watch('www/src/**/*.js', ['concat-js-main']);
 });
 
-gulp.task('watch-js-uglify', function() {
-    gulp.watch('www/dist/js/main.js', ['uglify-js']);
+gulp.task('watch-uglify-js-main', function() {
+    gulp.watch('www/dist/js/main.js', ['uglify-js-main']);
+});
+
+gulp.task('watch-concat-js-lib', function() {
+    gulp.watch(jslibs, ['concat-js-lib']);
+});
+
+gulp.task('watch-uglify-js-lib', function() {
+    gulp.watch('www/dist/js/dependencies.js', ['uglify-js-lib']);
 });
